d5e5b33847ae60ff7c70fead37e5c53d387fecb3
Michael A. Buice <mabuice@gmail.com>
2018-09-25 13:29:44 -0700

    added prepare_manifest

---
+++ visual_coding_2p_analysis/prepare_manifest.py
@@ -0,0 +1,28 @@
+
+
+
+from allensdk.core.brain_observatory_cache import BrainObservatoryCache
+
+TEMP_MANIFEST_FILE = 'data/manifest.json'
+
+
+def get_boc_json_files():
+    # We need 'base_uri' until all of the data is publicly released
+    boc = BrainObservatoryCache(manifest_file=TEMP_MANIFEST_FILE, base_uri="http://swarehouse/")
+
+    exps = boc.get_ophys_experiments()
+    cells = boc.get_cell_specimens()
+    ecs = boc.get_experiment_containers()
+
+# for i, exp in enumerate(exps):
+#     print("%d %d/%d" % (exp['id'], i+1, len(exps)))
+#     boc.get_ophys_experiment_data(exp['id'])
+#     boc.get_ophys_experiment_events(exp['id'])
+#     boc.get_ophys_experiment_analysis(ophys_experiment_id=exp['id'],
+#                                       stimulus_type='natural_movie_one')
+
+
+
+if __name__ == '__main__':
+
+    get_boc_json_files()
